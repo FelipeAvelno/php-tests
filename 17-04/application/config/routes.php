e3e844d9b5ab8dbb5f21b6d2699cc5ca3d26124b
Felipe Avelino <felipe.avelino10101@gmail.com>
2025-04-22 14:55:55 -0300

    Update routes.php

--- 17-04/application/config/routes.php
+++ 17-04/application/config/routes.php
@@ -50,5 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'teste';
+$route['dados_sensores/insert'] = 'DadosSensores/insert_new_reading';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
